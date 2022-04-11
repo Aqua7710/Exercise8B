b83c6bca3982f695b8c1208f536fad029e70d968
Aqua7710 <purdonwill@gmail.com>
2022-04-12 08:36:48 +1200

    Add project files.

---
+++ Exercise8B/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int counter, userInput;
+Console.Write("Hello user, this program will display a certain amount of even numbers depending on how many you want.\nHow many even numbers would you like printed? ");
+userInput = Convert.ToInt32(Console.ReadLine());  
+for (counter = 2; counter <= userInput * 2; counter++)
+{
+    if (counter % 2 == 0)
+    {
+        Console.WriteLine(counter.ToString());
+    }
+}
